ada3757d99dec8494c5363d4f3fb956bd6684a07
Laotan1123 <Laotan2300@gmail.com>
2025-10-06 22:31:16 +0100

    Prepare site for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/bon-appetit/',          // ensures correct asset paths on GH Pages
+  base: '/bon-appetit/',
   plugins: [react()],
   build: {
-    outDir: 'build'               // gh-pages will publish this folder
+    outDir: 'build'
   }
 });
